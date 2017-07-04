bd889d2c0b9fa737adf85a342f115c6d26d7cb10
(Davidson) Minsheng Zheng <nadavidsonzheng@gmail.com>
2017-07-04 10:23:34 -0400

    add initial scene

---
+++ Assets/Scripts/ResetApp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ResetApp : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene(0);
+        }
+
+        if (Input.GetKeyDown("1"))
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (Input.GetKeyDown("2"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
--- Assets/VRTK/Scripts/Pointers/VRTK_SimplePointer.cs
+++ Assets/VRTK/Scripts/Pointers/VRTK_SimplePointer.cs
@@ -68,7 +68,7 @@ namespace VRTK
                 SetPointerTransform(pointerBeamLength, pointerThickness);
                 if (rayHit)
                 {
-                    Debug.Log("Hit object! "+ rayHit);
+                    //Debug.Log("Hit object! "+ rayHit);
                     if (pointerCursorMatchTargetNormal)
                     {
                         pointerTip.transform.forward = -pointerCollidedWith.normal;
